e5d34fcbe3d3dc9287d21b6e42a70096c52da1f0
ybendris <yannis254@hotmail.fr>
2026-01-03 19:14:00 +0100

    ✨ clean app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [],
     templateUrl: './app.html',
     styleUrl: './app.scss',
 })
-export class App {
-    protected readonly title = signal('setup-angular');
-}
+export class App {}
